b7d12b5efc442967c69d0e4dda245b174232bd08
xliu12 <49348732+xliu12@users.noreply.github.com>
2021-04-11 08:32:45 -0400

    Add files via upload

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' A simulated observed dataset from a partially nested design.
+#'
+#' A dataset simulated from a A simulated observed data set from a partially nested design
+#' containing two types of pre-treatment confounders, treatment assignments,
+#' treatment cluster assignments, and outcome scores.
+#'
+#'
+#' @format A data frame with 200 rows and 5 variables:
+#' \describe{
+#'   \item{Trt}{Treatment assignment indicator (1 for treatment and 0 for control).}
+#'   \item{clus}{Observed treatment cluster assignment. clus = 0 for the control arm.}
+#'   \item{Y}{An outcome variable}
+#'   \item{Ly}{The first type of pre-treatment confounder that affects both the treatment assignment and outcome directly.}
+#'   \item{Lz}{The first type of pre-treatment confounder that does not affect the outcome in a given treatment cluster directly, but affects both the treatment assignment and treatment cluster assignment.}
+#' }
+#' @source \url{https://github.com/xliu12/IPWpn}
+"dat_obs"
